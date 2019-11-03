2b5faa0344eef97dbfabc895a3c5970132b2c0a9
Alexander Zinin <spore_09@mail.ru>
2019-11-04 00:02:06 +0900

    add base schedule render

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,53 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 import { render } from './pug';
+import { moment } from './services/moment';
+import { LocalScheduleBackend } from './scheduleServer/LocalScheduleBackend';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/*', (ctx) => {
-  ctx.body = render('index');
+const backend = new LocalScheduleBackend();
+
+// FIXME
+router.get('/style.css', (ctx) => {
+  ctx.type = 'text/css';
+  ctx.body = createReadStream(join(__dirname, 'styles', 'index.css'), {
+    encoding: 'utf-8',
+  });
+});
+
+router.get('/*', async (ctx) => {
+  const from = moment().subtract(3, 'days');
+  const to = moment();
+
+  const schedule = await backend.loadSchedule(
+    from.toISOString(),
+    to.toISOString(),
+    '4Z',
+  );
+
+  const range = moment.range(from, to);
+  const rangeDays = Array.from(range.by('days'));
+  // FIXME
+  const times = [
+    ['8:00', '9:30'],
+    ['9:40', '11:10'],
+    ['11:20', '12:50'],
+    ['13:00', '14:30'],
+    ['14:40', '16:10'],
+    ['16:20', '17:50'],
+    ['18:00', '19:30'],
+    ['19:40', '21:10'],
+  ];
+
+  ctx.body = render('index', { schedule, rangeDays, times });
 });
 
 app.use(router.routes());
 
-app.listen(3000);
+app.listen(3333);
 
 console.log('Server running on port 3000');
